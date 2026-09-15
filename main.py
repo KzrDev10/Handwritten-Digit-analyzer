7ce512ce16c515270938851c16df6bb2a0a6b729
KzrDev10 <rinnegan3646@gmail.com>
2026-09-15 23:01:55 +0100

    Add prediction logic and the screen listening part and instructions for users

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from turtle import Turtle
 from tkinter import Canvas
 from turtle import *
 import numpy as np
@@ -6,11 +7,8 @@ import tensorflow as tf
 #Loading model
 model = tf.keras.models.load_model('digit_classifier.keras')
 
-#setup teh grid so our model can use
+#setup the grid so our model can use
 grid = np.zeros((28,28))
-
-#turtle setup
-pen = Turtle()
 
 #screen setup
 screen = Screen()
@@ -20,6 +18,18 @@ screen.setup(width=560,height=560)
 screen.tracer(0)
 canvas = screen.getcanvas()
 
+#turtle setup
+pen = Turtle()
+
+text = Turtle()
+text.hideturtle()
+text.penup()
+text.goto(x=10,y=2)
+text.color("white")
+text.write("Draw a digit. Enter: Predict. C: Clear")
+
+
+#functions
 
 def paint(event):
     col = event.x // 20
@@ -29,7 +39,20 @@ def paint(event):
     pen.goto(col,row)
     pen.dot(20,"white")
 
-   
+def clear_canvas():
+    pen.clear()
+    grid.fill(0)
+
+def predict_num():
+    model_input = grid.reshape(1,28,28)
+    raw_predict = model.predict(model_input)
+    final_prediction = np.argmax(raw_predict)
+
+    print(f"The model predicts {final_prediction}")
 
 canvas.bind('<B1-Motion>',paint)
+
+screen.onkey(clear_canvas,"c")
+screen.onkey(predict_num, "Return")
+screen.listen()
 screen.mainloop()
